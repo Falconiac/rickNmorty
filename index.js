118ba17b22b9fbbaea202581e91d14154cf31b50
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-05-20 11:25:41 +0200

    Basic-structure done

---
+++ index.js
@@ -0,0 +1,4 @@
+const filterArea = document.querySelector("[data-js=filterAreaJS]");
+const cardArea = document.querySelector("[data-js=cardAreaJS]");
+const footer = document.querySelector("[data-js=footerJS]");
+const navBar = document.querySelector("[data-js=navBarJS]");
